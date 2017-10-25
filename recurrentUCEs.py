7ca08ea19e2ddc9d4d85fccb8a96a2a80aca902b
rmccole <rmccole@genetics.med.harvard.edu>
2017-10-25 14:48:35 -0400

    Update recurrentUCEs.py

--- recurrentUCEs.py
+++ recurrentUCEs.py
@@ -5,6 +5,20 @@ This script allows the user to check for recurrence of UCEs among one or more va
 from a master file which contains a unique identifier for each UCE, its coordinates and its type
 (exonic/intronic/intergenic). The variant files should be normal 3-column interval files.
 
+Copyright 2017 Harvard University, Wu Lab
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 """
 
 from __future__ import print_function
